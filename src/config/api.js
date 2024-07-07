e2f5d68cb8cc4a7418f99de24bbc6bcf9551708f
csbamba <114513598+csbamba@users.noreply.github.com>
2024-07-07 18:13:46 +0530

    Initial commit

---
+++ src/config/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const API_URL = "https://alluring-balance-production.up.railway.app";
+
+
+export const api = axios.create({
+  baseURL: API_URL, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
